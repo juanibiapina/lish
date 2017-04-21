c30a3a82e917512864b3e1638c0d8d72cc5ee586
Juan Ibiapina <juanibiapina@gmail.com>
2017-04-21 23:20:04 +0200

    feature: Display prompt and read one line

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use std::io;
+use std::io::Write;
+
 fn main() {
-    println!("Hello, world!");
+    print!(":) ");
+    io::stdout().flush().ok().expect("Could not flush stdout");
+
+    let mut line = String::new();
+
+    io::stdin().read_line(&mut line).expect("Could not read standard input");
 }
