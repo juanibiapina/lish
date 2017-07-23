74018d47490f5add97c4abf04398fa88a0a84fbf
Juan Ibiapina <juanibiapina@gmail.com>
2017-07-23 13:21:16 +0200

    feature: Add support for aliases in shell

--- src/evaluator.rs
+++ src/evaluator.rs
@@ -35,7 +35,7 @@ impl Evaluator {
     pub fn eval(&self, program: Program, env: Env) -> Result<Option<LispValue>> {
         match program {
             Program::ShellProgram(shell_expr) => {
-                self.eval_shell_expr(shell_expr)
+                self.eval_shell_expr(shell_expr, env)
             }
             Program::LispProgram(lisp_expr) => {
                 Ok(Some(self.eval_lisp_expr(lisp_expr, env)?))
@@ -46,8 +46,10 @@ impl Evaluator {
         }
     }
 
-    fn eval_shell_expr(&self, shell_expr: ShellExpr) -> Result<Option<LispValue>> {
-        let mut command = Command::new(&shell_expr.command);
+    fn eval_shell_expr(&self, shell_expr: ShellExpr, env: Env) -> Result<Option<LispValue>> {
+        let resolved_command = self.resolve_alias(&shell_expr.command, env)?;
+
+        let mut command = Command::new(&resolved_command);
         command.args(&shell_expr.args);
 
         let mut child = match command.spawn() {
@@ -67,6 +69,33 @@ impl Evaluator {
         child.wait()?;
 
         Ok(None)
+    }
+
+    fn resolve_alias(&self, name: &str, env: Env) -> Result<String> {
+        let value = env_get(&env, "ALIASES").ok();
+        match value {
+            Some(ref value) => {
+                match *value.clone() {
+                    LispType::HashMap(ref data) => {
+                        match data.get(name) {
+                            Some(value) => {
+                                match **value {
+                                    LispType::Strn(ref value_str) => Ok(value_str.to_owned()),
+                                    _ => Err(Error::TypeError),
+                                }
+                            },
+                            None => {
+                                Ok(name.to_owned())
+                            }
+                        }
+                    },
+                    _ => Err(Error::TypeError),
+                }
+            }
+            None => {
+                Ok(name.to_owned())
+            }
+        }
     }
 
     fn eval_lisp_expr(&self, lisp_expr: LispValue, env: Env) -> Result<LispValue> {
