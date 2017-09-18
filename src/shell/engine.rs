571d070f3aa14ebbf0a2063da6530ecf1040a917
Juan Ibiapina <juanibiapina@gmail.com>
2017-09-18 22:25:23 +0200

    refactor: Move alias expansion to lexical scope

--- src/shell/engine.rs
+++ src/shell/engine.rs
@@ -1,7 +1,9 @@
-use error::Result;
-use env::Env;
+use error::{Error, Result};
+use env::{Env, env_get};
+use types::LispType;
 
 use shell::parser::Parser;
+use shell::token::Token;
 use shell::lexer::tokenize;
 use shell::evaluator::Evaluator;
 
@@ -20,10 +22,74 @@ impl Engine {
 
     pub fn run(&mut self, input: &str, env: Env) -> Result<()> {
         let tokens = tokenize(input)?;
+
+        let tokens = resolve_alias(tokens, env)?;
+
         self.parser.add_tokens(tokens);
 
         let expr = self.parser.parse()?;
 
-        self.evaluator.eval(expr, env)
+        self.evaluator.eval(expr)
+    }
+}
+
+fn extract_head_for_alias(tokens: &[Token]) -> Option<String> {
+    match tokens[0] {
+        Token::Ident(ref name) => Some(name.to_owned()),
+    }
+}
+
+fn resolve_alias(tokens: Vec<Token>, env: Env) -> Result<Vec<Token>> {
+    let head = extract_head_for_alias(&tokens);
+
+    match head {
+        None => Ok(tokens),
+        Some(head) => {
+            let mapping = get_alias_mapping(&head, &env)?;
+
+            match mapping {
+                None => Ok(tokens),
+                Some(value) => {
+                    let mut words = vec![];
+
+                    words.push(Token::Ident(value));
+
+                    if let Some((_, args)) = tokens.split_first() {
+                        words.extend_from_slice(args);
+
+                        Ok(words)
+                    } else {
+                        panic!("impossible");
+                    }
+                }
+            }
+        }
+    }
+}
+
+fn get_alias_mapping(name: &str, env: &Env) -> Result<Option<String>> {
+    let value = env_get(env, "ALIASES").ok();
+    match value {
+        Some(ref value) => {
+            match *value.clone() {
+                LispType::HashMap(ref data) => {
+                    match data.get(name) {
+                        Some(value) => {
+                            match **value {
+                                LispType::Strn(ref value_str) => Ok(Some(value_str.to_owned())),
+                                _ => Err(Error::TypeError),
+                            }
+                        },
+                        None => {
+                            Ok(None)
+                        }
+                    }
+                },
+                _ => Err(Error::TypeError),
+            }
+        }
+        None => {
+            Ok(None)
+        }
     }
 }
--- src/shell/evaluator.rs
+++ src/shell/evaluator.rs
@@ -1,80 +1,17 @@
 use std::process::Command;
 use std::io::ErrorKind;
 
-use types::{LispType, ShellExpr};
-use env::{Env, env_get};
+use types::ShellExpr;
 use error::{Error, Result};
 
 pub struct Evaluator;
-
-fn get_alias_mapping(name: &str, env: &Env) -> Result<Option<String>> {
-    let value = env_get(env, "ALIASES").ok();
-    match value {
-        Some(ref value) => {
-            match *value.clone() {
-                LispType::HashMap(ref data) => {
-                    match data.get(name) {
-                        Some(value) => {
-                            match **value {
-                                LispType::Strn(ref value_str) => Ok(Some(value_str.to_owned())),
-                                _ => Err(Error::TypeError),
-                            }
-                        },
-                        None => {
-                            Ok(None)
-                        }
-                    }
-                },
-                _ => Err(Error::TypeError),
-            }
-        }
-        None => {
-            Ok(None)
-        }
-    }
-}
-
-fn resolve_alias(shell_expr: ShellExpr, env: Env) -> Result<ShellExpr> {
-    let mapping = get_alias_mapping(&shell_expr.words[0], &env)?;
-
-    match mapping {
-        Some(value) => {
-            let mut words = vec![];
-
-            words.push(value);
-
-            if let Some((_, args)) = shell_expr.words.split_first() {
-                words.extend_from_slice(args);
-
-                Ok(ShellExpr{
-                    words: words
-                })
-            } else {
-                panic!("impossible");
-            }
-        },
-        None => Ok(shell_expr),
-    }
-}
-
-fn make_command(shell_expr: &ShellExpr) -> Command {
-    if let Some((cmd, args)) = shell_expr.words.split_first() {
-        let mut command = Command::new(cmd);
-        command.args(args);
-        return command;
-    } else {
-        panic!("impossible");
-    }
-}
 
 impl Evaluator {
     pub fn new() -> Evaluator {
         Evaluator
     }
 
-    pub fn eval(&self, shell_expr: ShellExpr, env: Env) -> Result<()> {
-        let expr = resolve_alias(shell_expr, env)?;
-
+    pub fn eval(&self, expr: ShellExpr) -> Result<()> {
         let mut command = make_command(&expr);
 
         let mut child = match command.spawn() {
@@ -94,5 +31,15 @@ impl Evaluator {
         child.wait()?;
 
         Ok(())
+    }
+}
+
+fn make_command(shell_expr: &ShellExpr) -> Command {
+    if let Some((cmd, args)) = shell_expr.words.split_first() {
+        let mut command = Command::new(cmd);
+        command.args(args);
+        return command;
+    } else {
+        panic!("impossible");
     }
 }
