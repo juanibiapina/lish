83ac5eb67b045c6e0ed513fecfb047d5909a5c7a
Juan Ibiapina <juanibiapina@gmail.com>
2017-09-18 21:30:50 +0200

    refactor: Remove unused display implementation

--- src/lisp/token.rs
+++ src/lisp/token.rs
@@ -1,18 +1,6 @@
-use std::fmt;
-
 #[derive(PartialEq, Debug, Clone)]
 pub enum Token {
     LParen,
     RParen,
     Ident(String),
-}
-
-impl fmt::Display for Token {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match *self {
-            Token::LParen => write!(f, "("),
-            Token::RParen => write!(f, ")"),
-            Token::Ident(ref s) => write!(f, "{}", s),
-        }
-    }
 }
--- src/shell/token.rs
+++ src/shell/token.rs
@@ -1,14 +1,4 @@
-use std::fmt;
-
 #[derive(PartialEq, Debug, Clone)]
 pub enum Token {
     Ident(String),
-}
-
-impl fmt::Display for Token {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match *self {
-            Token::Ident(ref s) => write!(f, "{}", s),
-        }
-    }
 }
