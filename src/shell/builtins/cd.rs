4e964dcb88b0e185f24002f9aefb21ceb6bde0bc
Hidehito Yabuuchi <hdht.ybuc@gmail.com>
2017-10-02 08:42:32 +0900

    cd $HOME when no arguments are passed.

--- src/shell/builtins/cd.rs
+++ src/shell/builtins/cd.rs
@@ -4,7 +4,11 @@ use std::path::Path;
 use error::Result;
 
 pub fn run(args: Vec<String>) -> Result<()> {
-    let target = Path::new(&args[0]);
+    let target = if args.is_empty() {
+        env::home_dir().expect("Could not get your home dir")
+    } else {
+        Path::new(&args[0]).to_path_buf()
+    };
 
     env::set_current_dir(&target)?;
 
